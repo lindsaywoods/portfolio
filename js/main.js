74dbfcfd0ea19086971d0a539d8d6da858727454
Lindsay Woods <lindsaywoods@live.ca>
2018-03-11 18:22:31 -0400

    started scroll to top button in footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-"use strict";
+'use strict';
+
+$(document).ready(function () {
+
+  //Check to see where window is
+  // if relevant, display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
+$(document).ready(function () {
 
+  //Check to see where window is
+  // if relevant, display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+});
 
